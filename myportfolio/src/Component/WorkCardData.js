796a18c94ca762c2aa8b4c42f02b1128d0351239
Anshuman Chaturvedi <anshumanchaturvedi91@gmail.com>
2023-12-03 12:32:51 +0530

    first commit

---
+++ myportfolio/src/Component/WorkCardData.js
@@ -0,0 +1,20 @@
+import mkvcinemaproject from '../assets/mkvcinemaproject.webp'
+import Screenshot from '../assets/Screenshot .png';
+const ProjectCardData = [
+    {
+        imgsrc: mkvcinemaproject,
+        title:  "MKV Cinema Clone",
+        text:  "Added Navbar, Having Login & SignUp functionality, Fetched Data from Omdb Movies api , Added Cart functionality, Having basics website features",
+        view: "https://anshumanchaturvedidemo.netlify.app",
+        source: "https://github.com/masai-course/anshuman_chaturvedi_cap05_145/tree/master/tech/mkv%20cinema/mkvcinema/src"
+    },
+    {
+        imgsrc: Screenshot,
+        title:  "Real-Estate Website Clone",
+        text:  "Added Navbar,Having various navbar pages, added swiper functionality, Having responsive UI, Having footer component, Added Various React Icons Feature, Having Search functionality",
+        view: "https://656b10b878668a31257fd338--shiny-lollipop-c5df8a.netlify.app/",
+        source: "https://github.com/masai-course/anshuman_chaturvedi_cap05_145/commit/fb7f2f3e2afddab1029b0d2291f849e7723f62a4"
+    }
+]
+
+export default ProjectCardData;
